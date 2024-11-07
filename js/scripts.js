08a13b767e0ecae41d86b5bd4e34cf2d1974d9c4
sofilarrea <sofialejandra.larrea@gmail.com>
2024-11-06 22:08:04 -0300

    challenge teladoc

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
-//Add you JS here
+
+const buttons = document.querySelectorAll('.card-button');
+
+buttons.forEach(button => {
+  button.addEventListener('click', function() {
+    const card = this.closest('.card');
+
+    card.classList.toggle('closed');
+
+    const cardBody = card.querySelector('.card-body');
+
+    if (card.classList.contains('closed')) {
+      cardBody.style.display = 'none';
+      this.textContent = 'Show Text';
+    } else {
+      cardBody.style.display = 'block';
+      this.textContent = 'Hide Text';
+    }
+  });
+});
