707d14414c8b6da42dd2ea97967008a8b75c394b
justinait <justiiturraspe@gmail.com>
2024-11-08 16:51:16 -0300

    test

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-//Add you JS here
+function showText(button) {
+    
+    const cardBox = button.parentElement
+    const text = cardBox.querySelector(".text");
+    
+    text.style.display = text.style.display === "none" ? "block" : "none";
+    button.innerText = text.style.display === "none" ? "Show text" : "Hide text";
+    cardBox.style.backgroundColor = text.style.display === "none" ? "white" : "aliceblue";
+    
+}
